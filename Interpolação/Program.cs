cff091d0da4f8baa1dd10dfa8b91a8ff0183eea9
Augusto Omena <augusto.omena.n@gmail.com>
2023-02-08 10:05:53 -0300

    Adicionar arquivos de projeto.

---
+++ Interpolação/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Interpolacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2}");
+        }
+    }
+}
